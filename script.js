4d7644dffe8f1481e8438f8df5882aa184701e3f
rayosmaccquin <oliveira.santos.lucas2912@escola.pr.gov.br>
2025-12-12 21:23:43 +0000

    d

---
+++ script.js
@@ -0,0 +1,5 @@
+const caixaPrincipal = document.queryselector(".caixa-principal");
+const caixaPerguntas = document.queryselector(".caixa-perguntas");
+const caixaAlternativas = document.queryselector(".caixa-alternativas");
+const caixaResultado = document.queryselector(".caixa-resultado");
+const textoResultado = document.queryselector(".texto-resultado");
